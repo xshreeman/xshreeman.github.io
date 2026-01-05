03da93c255cd79341833bed610b5104107979604
Shreeman Tripathi <xshreeman@gmail.com>
2026-01-05 02:52:59 -0800

    Fix SvelteKit static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,20 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
-const dev = process.argv.includes("dev");
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [preprocess({ postcss: true })],
 
   kit: {
-    adapter: adapter({
-      // fallback: '404.html'
-    }),
-  },
-  paths: {
-    base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    adapter: adapter(),
+
+    prerender: {
+      entries: ["*"],
+    },
+
+    paths: {
+      base: "",
+    },
   },
 };
 
